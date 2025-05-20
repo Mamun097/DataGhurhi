a232ef44c6b4f3e522021339de12abf2af604b22
Tawhid Umar <tawhidumar.21@protonmail.com>
2025-05-20 17:25:17 +0600

    delete feature implemented

--- BSDT-User/route/surveytemplate.js
+++ BSDT-User/route/surveytemplate.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const surveyTemplateController = require('../controller/surveytemplatecontroller');
+const { jwtAuthMiddleware } = require('../auth/authmiddleware');
+
 
 // Route to create a survey template
 router.post('/', surveyTemplateController.createSurveyTemplate);
-
+router.delete('/:survey_id', jwtAuthMiddleware, surveyTemplateController.deleteSurveyForm);
 module.exports = router;
