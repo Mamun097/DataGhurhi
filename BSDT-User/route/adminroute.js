a7b13510043d6874ed45d75fbbda2cfee0ea590c
Mamun Munshi <mamunmunshi055@gmail.com>
2025-05-30 22:30:10 +0600

    survey analytics added

--- BSDT-User/route/adminroute.js
+++ BSDT-User/route/adminroute.js
@@ -9,4 +9,5 @@ router.put("/admin/update-package/:id", adminController.updatePackage);
 router.post("/admin/create-package", adminController.createPackage);
 router.get("/admin/most-popular-package", adminController.getMostPopularPackageId);
 router.get("/admin/user-growth-stats", adminController.getUserGrowthStats);
+router.get("/admin/survey-growth-stats", adminController.getSurveyGrowthStats);
 module.exports = router;
