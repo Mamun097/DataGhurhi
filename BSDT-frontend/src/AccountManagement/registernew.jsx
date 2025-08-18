274002215ac4f30ee60028365c6932c825740943
jinzi <swarnalisaha311220@gmail.com>
2025-08-18 20:20:39 +0600

    register problem solved

--- BSDT-frontend/src/AccountManagement/registernew.jsx
+++ BSDT-frontend/src/AccountManagement/registernew.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { motion } from "framer-motion";
 import "./register.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import apiClient from "../api";
