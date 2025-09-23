4b6b37c344dffaa7f3487f27f2f6c85b0b46e775
Mamun Munshi <mamunmunshi055@gmail.com>
2025-09-24 00:34:21 +0600

    coupon handling done

--- BSDT-User/server.js
+++ BSDT-User/server.js
@@ -23,7 +23,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Type', 'Authorization'],
 }));
